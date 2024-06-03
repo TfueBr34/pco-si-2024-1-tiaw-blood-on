bd3020dcfae71fa3a5a8c1522e52b36c57e4b486
Tfue <Samvit190@gmail.com>
2024-06-02 22:46:15 -0300

    Finalização da funcionalidade para a sprint 3

--- codigo/Assets/Scripts/medidor.js
+++ codigo/Assets/Scripts/medidor.js
@@ -1,10 +1,14 @@
+//Declaração da rota do JSON Server
 const url_sangue = "http://localhost:3000/tipos_sangue";
+
+//Função de coleta de informações do JSON Server dado o id do elemento
 async function get_info(url) {
     const res = await fetch(url);
   
     return res.json();
 }
 
+//Função para configurar a imagem correta na seção de estoque de sangue, baseado nos dados armazenados em JSON
 async function set_medidores(){
     images = document.querySelectorAll(".blood");
     for(let i = 0; i < images.length; i++){
@@ -13,15 +17,19 @@ async function set_medidores(){
     }
 }
 
-set_medidores();
-
+//Aguarda o conteúdo do html carregar para continuar
 window.addEventListener("DOMContentLoaded",function(){
+    set_medidores();
+
     let images = document.querySelectorAll(".blood");
+
+    //Para cada imagem encontrada na seção de estoque adiciona uma função onclick que cria uma caixa informativa sobre o tipo sanguíneo selecionado
     images.forEach(image =>{
         image.addEventListener("click", async function(){
             let box_description = document.getElementById("dialog");
             box_description.classList.remove("d-none");
             let tipo_sangue = await get_info(url_sangue+"?id="+image.id);
+            //Realiza o tratamento do nome do JSON
             let nome_extenso = tipo_sangue[0].nome_tipo.substr(0, tipo_sangue[0].nome_tipo.length-1) + " ";
     
             switch(tipo_sangue[0].nome_tipo.substr(tipo_sangue[0].nome_tipo.length-1, tipo_sangue[0].nome_tipo.length)){
@@ -35,7 +43,7 @@ window.addEventListener("DOMContentLoaded",function(){
                     console.log("Erro ao catalogar nome do tipo sanguíneo!");
                     break;
             }
-    
+            //Determina o nome do estado atual do estoque
             switch(tipo_sangue[0].id_categoria_qt){
                 case 1:
                     estado = "Crítico";
@@ -64,19 +72,21 @@ window.addEventListener("DOMContentLoaded",function(){
                         <hr>
                         <h4>Momento recomendado para doar:</h4><h6>${tipo_sangue[0].dsc_categoria}</h6>
                         <hr>
-                        <h4>Dados atualizados em:</h4><h6>${tipo_sangue[0].data_att}  <i class="fa-solid fa-plus" ondblclick="add_border()"></i></h6>
+                        <h4>Dados atualizados em:</h4><h6>${tipo_sangue[0].data_att}  <i class="fa-solid fa-plus" ondblclick="saiba_mais()"></i></h6>
                     </div>`;
             box_description.innerHTML = html_description;
         },false);
     });
 },false);
 
+//Função que fecha o painel informativo
 function close_dialog(){
     let box_description = document.getElementById("dialog");
     box_description.classList.add("d-none");
 }
 
-function add_border(){
+//Função que substitui o painel atual por uma explicação da origem dos dados do estoque
+function saiba_mais(){
     let box_description = document.getElementById("dialog");
     let html_description = `<div class="description">
                                 <div class="text-end">
