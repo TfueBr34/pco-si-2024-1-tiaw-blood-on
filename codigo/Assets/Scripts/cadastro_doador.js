993c946e7ae3589744b530c5b88ff2d7c3b18a3e
Tfue <Samvit190@gmail.com>
2024-06-29 19:28:25 -0300

    Adição da funcionalidade de cadastro e correção da responsividade

--- codigo/Assets/Scripts/cadastro_doador.js
+++ codigo/Assets/Scripts/cadastro_doador.js
@@ -24,26 +24,53 @@
 
 const url_usuario = "http://localhost:3000/usuarios";
 
-function salva_doador(){
-    if(document.getElementById('telefone').value){
+//Função de coleta de informações do JSON Server dado o id do elemento
+async function get_info(url) {
+    const res = await fetch(url);
+  
+    return res.json();
+}
 
+async function salva_doador(){
+    if(document.getElementById("registrationForm").checkValidity()){
+        let usuarios = await get_info(url_usuario);
+        let last_id = 0;
+        let has_mail;
+        if(usuarios != null){
+            usuarios.forEach(usuario => {
+                if(document.getElementById('email').value.toLowerCase() == usuario.email){
+                    has_mail = true;
+                }
+                last_id = parseInt(usuario.id);
+            });
+        }
+        if(has_mail){
+            alert("Email já está cadastrado no site");
+        }else{
+            if(document.getElementById('senha').value != document.getElementById('repetirSenha').value){
+                alert("Senhas digitadas devem ser iguais");
+            }else{
+                last_id++;
+                let userData =  JSON.stringify({
+                    id: last_id.toString(),
+                    username: document.getElementById('name').value,
+                    email: document.getElementById('email').value.toLowerCase(),
+                    senha: document.getElementById('senha').value,
+                    bairro: document.getElementById('bairro').value.toLowerCase(),
+                    cidade: document.getElementById('cidade').value.toLowerCase(),
+                    dt_nasc: document.getElementById('dt_nasc').value,
+                    telefone: document.getElementById('telefone').value,
+                    sexo: document.querySelector('input[name="sexo"]:checked').value,
+                    url_pic: ""
+                });
+                fetch(url_usuario,{
+                    method: "POST",
+                    headers:{
+                        "Content-type": "aplication/json"
+                    },
+                    body: userData
+                }).then(res => res.json()).then(() => location.assign("../index.html"));
+            }
+        }
     }
-    let userData = {
-        nome: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        senha: document.getElementById('senha').value,
-        bairro: document.getElementById('bairro').value,
-        cidade: document.getElementById('cidade').value,
-        dt_nasc: document.getElementById('dt_nasc').value,
-        telefone: document.getElementById('telefone').value,
-        sex: document.querySelector('input[name="sexo"]:checked').value
-    };
-
-    fetch(url_usuario,{
-        method: "POST",
-        headers:{
-            "Content-type": "aplication/json"
-        },
-        body: userData
-    }).then(res => res.json()).then(() => location.reload());
 }
